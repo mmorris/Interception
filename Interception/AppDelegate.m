ba231e20695adbb50cc76b948b279451e62d9425
Matt Morris <matt@mattmorr.is>
2012-08-22 21:35:42 -0500

    minor + README

--- Interception/AppDelegate.m
+++ Interception/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  Interception
-//
-//  Created by matt on 7/26/12.
-//  Copyright (c) 2012 Matt Morris. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Interception
 ============
+This is just a simple hack wrapped in a .app bundle to trick Mountain Lion Twitter notifiations into launching native Twitter clients.
